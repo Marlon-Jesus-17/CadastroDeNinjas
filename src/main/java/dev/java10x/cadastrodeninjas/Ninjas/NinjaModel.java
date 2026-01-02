d3fe36447de182c757a0d3105dc1ca23c5cfdb09
Marlon <maarlonsantos17@gmail.com>
2026-01-02 12:43:28 -0300

    feat: Coloca o lombok na classe para abstracao

--- src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -2,11 +2,16 @@ package dev.java10x.cadastrodeninjas.Ninjas;
 
 import dev.java10x.cadastrodeninjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 
 @Entity //Entity transforma uma classe em uma entidade do banco de dados
 @Table(name = "tb_cadastro")    //Passar um nome pra tabela
+@NoArgsConstructor //Cria um contrutor vazio automaticamente
+@AllArgsConstructor //Cria um contrutor com todos os argumentos automaticament
+@Data //Cria todos os getters e setters automaticamente
 public class NinjaModel {
 
     @Id //Para trasformar a variavel logo abaixo em um Id
@@ -23,35 +28,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel() {}
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
